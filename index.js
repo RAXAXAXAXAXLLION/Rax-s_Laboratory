07f70d68b552d5de7ce13f254b547ae029911167
CrizzzSombiii <cristianstein555@gmail.com>
2017-09-05 17:48:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require("socket.io").listen(
 require("http").createServer((i,o)=>{
 o.writeHead(200,{"content-type":"text/html"})
 o.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>')
 o.write('<script>io().on("msg",(e)=>{eval(e.script)})</script>')
 o.end()
 }).listen(process.env.PORT||3000,()=>{console.log("Server listening.")})
+).on("connection",(w)=>{
+w.send("msg",{script:'alert("Hello user!")'})
+})
