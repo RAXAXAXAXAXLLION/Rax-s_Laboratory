a7841bf293f38e53da816ace5c669928d9e6274c
CrizzzSombiii <cristianstein555@gmail.com>
2017-09-06 16:12:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ o.write('<script>io().on("msg",(e)=>{eval(e.script)})</script>')
 o.end()
 }).listen(process.env.PORT||3000,()=>{console.log("Server listening.")})
 ).on("connection",(w)=>{
+w.c={r:0,g:0,b:0}
 w.emit("msg",{script:'document.body.style.margin="0"'})
 w.emit("msg",{script:'c=document.createElement("canvas")'})
 w.emit("msg",{script:'document.body.appendChild(c)'})
@@ -16,11 +17,11 @@ w.emit("msg",{script:'c.width=innerWidth'})
 w.emit("msg",{script:'c.height=innerHeight'})
 for(x=0;x<32;x++){
 for(y=0;y<32;y++){
-w.emit("msg",{script:'new Uint8Array(img.data.buffer)[0+'+x+'*4+'+y+'*4*32]=255'})
-w.emit("msg",{script:'new Uint8Array(img.data.buffer)[1+'+x+'*4+'+y+'*4*32]=0'})
-w.emit("msg",{script:'new Uint8Array(img.data.buffer)[2+'+x+'*4+'+y+'*4*32]=0'})
+w.emit("msg",{script:'new Uint8Array(img.data.buffer)[0+'+x+'*4+'+y+'*4*32]='+w.c.r})
+w.emit("msg",{script:'new Uint8Array(img.data.buffer)[1+'+x+'*4+'+y+'*4*32]='+w.c.g})
+w.emit("msg",{script:'new Uint8Array(img.data.buffer)[2+'+x+'*4+'+y+'*4*32]='+w.c.b})
 w.emit("msg",{script:'new Uint8Array(img.data.buffer)[3+'+x+'*4+'+y+'*4*32]=255'})
 }}
 w.emit("msg",{script:'ctx.putImageData(img,0,0)'})
-},1)
+},500)
 })
