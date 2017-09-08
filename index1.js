a874d73cef32447935210ab3c8ecea5023934397
CrizzzSombiii <cristianstein555@gmail.com>
2017-09-08 14:41:19 +0200

    Create index1.js

---
+++ index1.js
@@ -0,0 +1,23 @@
+require("socket.io").listen(
+require("http").createServer((i,o)=>{
+o.writeHead(200,{"content-type":"text/html"})
+o.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>')
+o.write('<script>io().on("msg",(e)=>{eval(e.script)})</script>')
+o.end()
+}).listen(process.env.PORT||3000,()=>{console.log("Server listening.")})
+).on("connection",(w)=>{
+w.c={r:0,g:0,b:0}
+w.emit("msg",{script:'document.body.style.margin="0"'})
+w.emit("msg",{script:'c=document.createElement("canvas")'})
+w.emit("msg",{script:'document.body.appendChild(c)'})
+w.emit("msg",{script:'ctx=c.getContext("2d")'})
+setInterval(()=>{
+w.emit("msg",{script:'c.width=innerWidth'})
+w.emit("msg",{script:'c.height=innerHeight'})
+for(x=0;x<8000;x++){
+for(y=0;y<8000;y++){
+w.emit("msg",{script:'ctx.fillStyle="rgba('+w.c.r+','+w.c.g+','+w.c.b+',1)"'})
+w.emit("msg",{script:'ctx.fillRect('+x+','+y+',1,1)'})
+}}
+},1000)
+})
