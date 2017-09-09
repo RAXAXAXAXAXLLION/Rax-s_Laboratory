07bfadbf155217516087bbd441116e48e5a09550
CrizzzSombiii <cristianstein555@gmail.com>
2017-09-09 21:39:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ o.writeHead(200,{"content-type":"text/html"})
 o.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>')
 o.write('<script>io().on("msg",(e)=>{eval(e.script)})</script>')
 o.end()
-}).listen("1025",()=>{console.log("Server listening.")})
+}).listen(process.env.PORT||3000,()=>{console.log("Server listening.")})
 /*).on("connection",(event)=>{
 event.id=Math.floor(Math.random()*Math.pow(10,10))
 event.emit("msg",{script:'k=[];m=[]'})
