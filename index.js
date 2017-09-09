5a78c0a20f620b9f7279d40b93d677770a66db51
CrizzzSombiii <cristianstein555@gmail.com>
2017-09-09 21:34:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-require("socket.io").listen(
-require("http").createServer((i,o)=>{
+require('socket.io').listen(
+require('http').createServer((i,o)=>{
 o.writeHead(200,{"content-type":"text/html"})
 o.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>')
 o.write('<script>io().on("msg",(e)=>{eval(e.script)})</script>')
 o.end()
-}).listen(process.env.PORT||3000,()=>{console.log("Server listening.")})
-).on("connection",(w)=>{
-w.c={r:0,g:0,b:0}
-w.emit("msg",{script:'document.body.style.margin="0"'})
-w.emit("msg",{script:'c=document.createElement("canvas")'})
-w.emit("msg",{script:'document.body.appendChild(c)'})
-w.emit("msg",{script:'ctx=c.getContext("2d")'})
-setInterval(()=>{
-w.emit("msg",{script:'c.width=innerWidth'})
-w.emit("msg",{script:'c.height=innerHeight'})
-for(x=0;x<4;x++){
-for(y=0;y<4;y++){
-w.emit("msg",{script:'ctx.fillStyle="rgba('+w.c.r+','+w.c.g+','+w.c.b+',1)"'})
-w.emit("msg",{script:'ctx.fillRect('+x+','+y+',1,1)'})
-}}
-},1000)
+}).listen("1025",()=>{console.log("Server listening.")})
+).on("connection",(event)=>{
+event.id=Math.floor(Math.random()*Math.pow(10,10))
+event.emit("msg",{script:'k=[];m=[]'})
+event.emit("msg",{script:'document.body.style.margin="0"'})
+event.emit("msg",{script:'onkeydown=onkeyup=(e)=>{k[e.keyCode]=e.type="keydown"}'})
+event.emit("msg",{script:'onmousedown=onmouseup=(e)=>{m[e.button]=e.type="mousedown"}'})
+event.emit("msg",{script:
+'onmousemove=(event)=>{'+
+'mouse_x:event.clientX;'+
+'mouse_y:event.clientY}'})
+event.emit("msg",{script:
+'setInterval(()=>{for(i=0;i<400;i++){'+
+'if(k[i]){io().emit("msg",{key:i})};'+
+'if(m[i]){io().emit("msg",{button:i})};'+
+'io().emit("msg",{x:mouse_x;y:mouse_y})'
+'}},1)'})
+event.on("msg",(e)=>{
+console.log("Received keyboard key: "+e.key+" from window: "+event.id)
+console.log("Received mouse button: "+e.button+" from window: "+event.id)
+console.log("Received mouse X: "+e.x+" from window: "+event.id)
+console.log("Received mouse Y: "+e.y+" from window: "+event.id)
+})
 })
