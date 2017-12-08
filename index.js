6ef27be8cbdf48a25dfb08caa5e96603d2547b40
CrizzzSombiii <cristianstein555@gmail.com>
2017-12-08 13:08:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,28 +16,28 @@ socket.emit("msg",{script:'c=document.createElement("canvas")'})
 socket.emit("msg",{script:'document.body.appendChild(c)'})
 socket.emit("msg",{script:'ctx=c.getContext("2d")'})
 socket.emit("msg",{script:'onkeydown=onkeyup=(e)=>{k[e.keyCode]=e.type=="keydown"}'})
-socket.emit("msg",{script:'setInterval(()=>{for(i=0;i<400;i++){if(k[i]){socket.emit("key",{code:i})}}},25)'},1)
+socket.emit("msg",{script:'setInterval(()=>{for(i=0;i<400;i++){if(k[i]){socket.emit("key",{code:i})}}},1)'})
 socket.on("key",(e)=>{
-console.log(e.code+" from "+socket.i)
-if(e.code==37){obj[socket.i].xs-=0.01}
-if(e.code==38){obj[socket.i].ys-=0.01}
-if(e.code==39){obj[socket.i].xs+=0.01}
-if(e.code==40){obj[socket.i].ys+=0.01}
+if(e.code==37){obj[socket.i].xs-=0.001}
+if(e.code==38){obj[socket.i].ys-=0.001}
+if(e.code==39){obj[socket.i].xs+=0.001}
+if(e.code==40){obj[socket.i].ys+=0.001}
 })
 setInterval(()=>{
 socket.emit("msg",{script:'c.width=innerWidth'})
 socket.emit("msg",{script:'c.height=innerHeight'})
 for(i in obj){
-socket.emit("msg",{script:'ctx.fillStyle="rgba(0,0,0,1)"'})
-socket.emit("msg",{script:'ctx.fillRect('+obj[i].x+','+obj[i].y+',32,32)'})
-}},50)
+socket.emit("msg",{script:'ctx.fillStyle="rgba(250,200,150,1)"'})
+socket.emit("msg",{script:'ctx.fillRect('+eval(obj[i].x-obj[socket.i].x)+'+innerWidth/2-16,'+eval(obj[i].y-obj[socket.i].y)+'+innerHeight/2-16,32,32)'})
+socket.emit("msg",{script:'ctx.lineWidth="4"'})
+socket.emit("msg",{script:'ctx.fillStyle="rgba(200,175,150,1)"'})
+socket.emit("msg",{script:'ctx.fillRect('+eval(obj[i].x-obj[socket.i].x)+'+innerWidth/2-16,'+eval(obj[i].y-obj[socket.i].y)+'+innerHeight/2-16,32,32)'})
+}},20)
 })
 setInterval(()=>{
 for(i in obj){
 obj[i].x+=obj[i].xs
 obj[i].y+=obj[i].ys
-if(obj[i].x<-32){obj[i].x=innerWidth+32}
-if(innerWidth+32<obj[i].x){obj[i].x=-32}
-if(obj[i].y<-32){obj[i].y=innerHeight+32}
-if(innerHeight+32<obj[i].y){obj[i].y=-32}
+obj[i].xs*=0.999
+obj[i].ys*=0.999
 }},1)
