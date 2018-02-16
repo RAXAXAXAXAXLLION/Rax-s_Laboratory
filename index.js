600b318df44acef131d170e4ad463de40359344f
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2018-02-17 00:42:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 app=require("express")()
 net=require("http").createServer(app)
 app.get("/",(req,res)=>{
-res.sendFile(__dirname+"/public/public.htm")
+res.sendFile(__dirname+"/docs/index.htm")
 })
-app.use("/public",require("express").static(__dirname+"/public"))
+app.use("/docs",require("express").static(__dirname+"/docs"))
 net.listen(process.env.PORT||3000,()=>{console.log("Ready!")})
 require("socket.io")(net,{}).sockets.on("connection",(socket)=>{
 console.log("socket connection")
